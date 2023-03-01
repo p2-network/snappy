387544eb8dda3284674c7ed86547bd965d0549eb
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2023-03-01 21:43:55 +1100

    chore(things): remove ubuntuContainer

--- things/index.ts
+++ things/index.ts
@@ -4,11 +4,11 @@ import * as docker from "@pulumi/docker";
 
 import "./src/ytdl";
 
-// Find the latest Ubuntu precise image.
-const ubuntuRemoteImage = new docker.RemoteImage("ubuntuRemoteImage", {
-  name: "ubuntu:precise"
-});
-// Start a container
-const ubuntuContainer = new docker.Container("ubuntuContainer", {
-  image: ubuntuRemoteImage.imageId
-});
+// // Find the latest Ubuntu precise image.
+// const ubuntuRemoteImage = new docker.RemoteImage("ubuntuRemoteImage", {
+//   name: "ubuntu:precise"
+// });
+// // Start a container
+// const ubuntuContainer = new docker.Container("ubuntuContainer", {
+//   image: ubuntuRemoteImage.imageId
+// });
